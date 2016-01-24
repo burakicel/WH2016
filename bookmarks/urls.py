ba418feb54fce56c756fdabbb83d97f2486913fa
Burak Icel <burakicel96@gmail.com>
2016-01-24 07:18:55 -0500

    main page and main functionalities

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
+    url(r'^$', 'home.views.index')
 ]
